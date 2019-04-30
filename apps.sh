9a327182db5d0c16a3905b65e36be8e9df8edbf8
Antoni Florit Homar <toniflorithomar@gmail.com>
2019-04-30 20:34:14 +0200

    Validate if software is installed or not

--- apps.sh
+++ apps.sh
@@ -2,48 +2,75 @@
 
 set -e
 
+sudo=''
+if [ "$EUID" -ne 0 ]; then
+    sudo="sudo"
+fi
+
 # Editors
 ## VSCODE
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt updates &&  apt install code
-code --install-extension shan.code-settings-sync
+if ! [ -x "$(command -v code )" ]; then
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    $sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    $sudo apt updates &&  apt install code -y
+    code --install-extension shan.code-settings-sync
+fi
 
 # Git desktop app
-snap install gitkraken
+if ! [ -x "$(command -v gitkraken )" ]; then
+    $sudo snap install gitkraken
+fi
 
 # Communication
-snap install slack
+if ! [ -x "$(command -v slack )" ]; then
+    $sudo snap install slack
+fi
 
 # Docker
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update && apt install docker-ce
-sudo systemctl status docker
-sudo usermod -aG docker $USER
+if ! [ -x "$(command -v docker )" ]; then
+    $sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    $sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    $sudo apt update && apt install -y docker-ce
+    $sudo systemctl status docker
+    $sudo usermod -aG docker $USER
+fi
 
 # Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+if ! [ -x "$(command -v docker-composer )" ]; then
+    $sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    $sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 # Postman
-sudo snap install postman
+if ! [ -x "$(command -v postman )" ]; then
+    $sudo snap install postman
+fi
 
 # Spotify
-snap install spotify
+if ! [ -x "$(command -v spotify )" ]; then
+    $sudo snap install spotify
+fi
 
 # HTOP
-sudo apt install htop
+if ! [ -x "$(command -v htop )" ]; then
+    $sudo apt install -y htop
+fi
 
 # Browsers
 ## Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo gdebi google-chrome-stable_current_amd64.deb
+if ! [ -x "$(command -v chome )" ]; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    $sudo gdebi google-chrome-stable_current_amd64.deb
+fi
 
 ## Firefox
-snap install firefox
+if ! [ -x "$(command -v firefox )" ]; then
+    snap install firefox
+fi
 
 ## Zoom
-wget -O /tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
-sudo apt update && sudo apt install libxcb-xtest0
-sudo dpkg -i /tmp/zoom.deb
+if ! [ -x "$(command -v zoom )" ]; then
+    wget -O /tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+    $sudo apt update && sudo apt install -y libxcb-xtest0
+    $sudo dpkg -i /tmp/zoom.deb
+fi
--- main.sh
+++ main.sh
@@ -2,26 +2,35 @@
 
 set -e
 
+sudo=''
+if [ "$EUID" -ne 0 ]; then
+    sudo="sudo"
+fi
+
 ## UPDATE
-sudo apt update
-sudo apt upgrade -y
+$sudo apt update
+$sudo apt upgrade -y
 
 ## BASE INSTALL
-sudo apt install software-properties-common git ca-certificates curl gdebi-core build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget apt-transport-https
+$sudo apt install -y software-properties-common git ca-certificates curl gdebi-core build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget apt-transport-https
 
 ## INSTALL PYTHON 3.7
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
+if ! [ -x "$(command -v python3.7 )" ]; then
+    $sudo add-apt-repository ppa:deadsnakes/ppa
+    $sudo apt install -y python3.7
+fi
 
 ## Python
-sudo apt install python-pip python3-pip
-pip install pipenv
-pip3 install pipenv
+if ! [ -x "$(command -v pip )" || -x "$(command -v pip3 )" ]; then
+    $sudo apt install -y python-pip python3-pip
+fi
+$sudo pip install pipenv
+$sudo pip3 install pipenv
 
 # Powerline
-pip install --user git+git://github.com/powerline/powerline
-pip install --user powerline-status
-pip install --user ipython
+$sudo pip install --user git+git://github.com/powerline/powerline
+$sudo pip install --user powerline-status
+$sudo pip install --user ipython
 
 # vim runtime
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
